7603672522ef3c93ed1600da41fe2fe78aebc9d6
Ryan Esteves <snargledorf@gmail.com>
2012-01-23 14:07:38 -0500

    Pull request prep

--- RESTduino.ino
+++ RESTduino.ino
@@ -18,7 +18,7 @@
  
  */
 
-#define DEBUG 1
+//#define DEBUG 1
 
 #include <SPI.h>
 #include <Ethernet.h>
@@ -26,7 +26,7 @@
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-byte ip[] = {192,168,1,178};
+byte ip[] = {192,168,1,177};
 
 // Initialize the Ethernet server library
 // with the IP address and port you want to use 
