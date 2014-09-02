b01cc7081ade5eac24c55655048aeacc99484f66
Jason J. Gullickson <jason.gullickson@gmail.com>
2014-09-02 06:22:09 -0500

    expanded bandwidth monitor example to set LED indicators

--- examples/python/monitor.py
+++ examples/python/monitor.py
@@ -8,10 +8,13 @@ from time import sleep
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 
 # config
-router_address = '10.0.1.1'
+restduino_address = '10.0.1.3'
+router_address = '10.1.10.1'
 router_communitystring = 'public'
-interface_oid = '1.3.6.1.2.1.2.2.1.10.5' # Apple Airport Extreme vlan0
+interface_oid = '1.3.6.1.2.1.31.1.1.1.6.15' # '1.3.6.1.2.1.2.2.1.10.5' # Apple Airport Extreme vlan0
 gauge_max = 50 # if the meter has an existing scale, this is the largest number
+warn_threshold = 100 # when to turn on yellow led (currently pwm value, should be gauge value) 
+danger_threshold = 200 # when to turn on red led (currently pwm value, should be gauge value) 
 gauge_update_threshold = 10 # how often (in seconds) to send an update to RESTDuino (10 is safe)
 
 # globals
@@ -19,6 +22,12 @@ last_sample = 0
 gauge_update_counter = 0
 value_avg_accumulator = 0
 		
+# turn on green led
+conn = httplib.HTTPConnection(restduino_address)
+gauge_url = '/14/HIGH'
+conn.request('GET', gauge_url)
+conn.close()
+
 while True:
 
 	cmdGen = cmdgen.CommandGenerator()
@@ -51,13 +60,39 @@ while True:
 			print('%f octets, %f megabits, %d scale' % (this_value, this_value_megabits, value_avg))
 
 			# update restduino
-			conn = httplib.HTTPConnection('10.0.1.3')
+			conn = httplib.HTTPConnection(restduino_address)
 			gauge_url = '/5/%d' % value_avg 
+			conn.request('GET', gauge_url)
+			conn.close()
+
+			# set led indicators
+			# need to wait between requests or we may overwhelm RESTduino
+			sleep(1)
+
+			# set yellow led 
+ 			if value_avg > warn_threshold:
+				gauge_url = '/15/HIGH'
+			else:
+				gauge_url = '/15/LOW'
+
+			conn = httplib.HTTPConnection(restduino_address)
+			conn.request('GET', gauge_url)
+			conn.close()
+
+			sleep(1)
+
+			# set red led
+			if value_avg > danger_threshold:
+				gauge_url = '/16/HIGH'
+			else:
+				gauge_url = '/16/LOW'
+
+			conn = httplib.HTTPConnection(restduino_address)
 			conn.request('GET', gauge_url)
 			conn.close()
 		
 		except:
-			print('error taking or displaying reading, waiting for next round')
+			print('error displaying reading, waiting for next round')
 
 		gauge_update_counter = 0
 		value_avg_accumulator = 0
